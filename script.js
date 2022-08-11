855df0352eca69d0f558d3131c366184cdad2df2
j-irion <juliusirion@gmx.de>
2022-08-11 20:12:14 +0200

    Create show and setBoard function, add test code

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
 const gameboard = (() => {
-  const gameboardArray = new Array(3);
-  gameboardArray.forEach((elem) => (elem = new Array(3)));
+  let gameboardArray = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+
+  const setBoard = (array) => {
+    gameboardArray = array;
+  };
+
+  const show = () => {
+    document.querySelector(".gameboard").innerHTML = "";
+    for (i = 0; i < 3; ++i) {
+      for (j = 0; j < 3; j++) {
+        let field = document.createElement("div");
+        field.textContent = gameboardArray[i][j];
+        document.querySelector(".gameboard").appendChild(field);
+      }
+    }
+  };
+
+  return {
+    show,
+    setBoard,
+    gameboardArray,
+  };
 })();
+
+const test = [
+  ["x", "o", "x"],
+  ["o", "x", "o"],
+  ["o", "o", "x"],
+];
+
+gameboard.setBoard(test);
+console.log(gameboard.gameboardArray);
+console.log(gameboard.gameboardArray[0][0]);
+gameboard.show();
